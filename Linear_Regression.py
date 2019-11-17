3fcb35c87dbe7d6b0c87ee15cb76aeed7a37dcc9
Akhil Mokkapati <41456005+SilverGun92@users.noreply.github.com>
2019-11-17 00:42:12 -0500

    regression with regularization
    regression with regularization

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -10,31 +10,34 @@ from sklearn.datasets import load_boston as lb
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-def compute_cost(X,y,theta):
+def compute_cost(X,y,theta,lamda):
+    theta_reg = np.vstack([0, theta[1:]])
     m=len(y)
     h= X @ theta
-    J =(1/(2*m))*np.sum((h-y)**2)
+    J =(1/(2*m))*np.sum((h-y)**2) + (lamda/(2*m))*np.sum(theta_reg**2)
     
     return J
 
 #Gradient descent baseed on no. of iterations
-def gradient_descent(X,y,theta,alpha,n_iters):
+def gradient_descent(X,y,theta,alpha,lamda,n_iters):
+    theta_reg = np.vstack([0, theta[1:]])
     m=len(y)
     J_history =np.zeros((n_iters,1))
     for i in range(n_iters):
-        theta = theta - (alpha/m)* X.T @ (X @ theta-y)
-        J_history[i] = compute_cost(X,y,theta)
+        theta = theta - (alpha/m)* X.T @ (X @ theta-y) - (alpha*lamda/m)*theta_reg
+        J_history[i] = compute_cost(X,y,theta,lamda)
     return(J_history,theta)
 
 #Gradient descent baseed on convergence limit
-def gradient_descent_conv(X,y,theta,alpha,conv_limit):
+def gradient_descent_conv(X,y,theta,alpha,lamda,conv_limit):
+    theta_reg = np.vstack([0, theta[1:]])
     m=len(y)
-    initial_cost = compute_cost(X,y,theta)
+    initial_cost = compute_cost(X,y,theta,lamda)
     J_history =np.array(initial_cost)
     delta=initial_cost
     while delta >= conv_limit:
-        theta = theta - (alpha/m)* X.T @ (X @ theta-y)
-        J_history = np.append(J_history,compute_cost(X,y,theta))
+        theta = theta - (alpha/m)* X.T @ (X @ theta-y)- (alpha*lamda/m)*theta_reg
+        J_history = np.append(J_history,compute_cost(X,y,theta,lamda))
         delta = J_history[-2]-J_history[-1]
     return(J_history,theta)
    
@@ -70,13 +73,13 @@ X = np.hstack((np.ones((m,1)),X))
 n_f = np.size(X,1)
 #Initializing theta's
 theta = np.zeros((n_f,1))
-
+lamda =0
 alpha =.01
 n_iters = 1500
 conv_limit = 0.00001
 # Solving Theta 
-#(J_history,theta) = gradient_descent(X,y,theta,alpha,n_iters)
-(J_history,theta) = gradient_descent_conv(X,y,theta,alpha,conv_limit)
+#(J_history,theta) = gradient_descent(X,y,theta,alpha,lamda,n_iters)
+(J_history,theta) = gradient_descent_conv(X,y,theta,alpha,lamda,conv_limit)
 
 # plotting cost function
 plot_cost(J_history)
